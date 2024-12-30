a5f29b77cfe389ab8ef78e62fd87eea5ce9141d0
Sethu Ravichandran <sethu@Sethus-MacBook-Air.local>
2024-12-30 16:49:49 +0530

    improvised the button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,34 @@
 import {FC} from 'react'
+import './Button.css'
 
-type Props = {
-    text:string
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary'
 }
 
-const Button:FC<Props> = ( {text} ) => {
+const Button:FC<ButtonProps> = ( {variant, ...props} ) => {
+
+  const styles = () => {
+    const propStyle = props.style ?? {}
+    if (props.disabled) return propStyle
+    if (variant === 'primary') return {
+      borderColor : '#6366F1',
+      backgroundColor : '#6366F1',
+      color : '#FAFAFA',
+      ...propStyle
+    }
+    if (variant === 'secondary') return {
+      borderColor : '#6366F1',
+      backgroundColor : '#FAFAFA',
+      color : '#6366F1',
+      ...propStyle
+    }
+    return propStyle
+  }
+
   return (
-    <div>{text}</div>
+    <button {...props} style={styles()}>
+      {props.children}
+    </button>
   )
 }
 
