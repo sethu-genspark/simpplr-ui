009f76bbb9185fb39e542991ad77795c54e3cbe8
Sethu Ravichandran <sethu@Sethus-MacBook-Air.local>
2024-12-30 16:27:45 +0530

    first commit

---
+++ rollup.config.mjs
@@ -0,0 +1,104 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
+import packageJson from './package.json' with { type: 'json' };
+
+// export default [
+//   {
+//     input: 'src/index.ts',
+//     output: [
+//       {
+//         file: 'dist/cjs/index.js',
+//         format: 'cjs',
+//         sourcemap: true,
+//       },
+//       {
+//         file: 'dist/esm/index.js',
+//         format: 'esm',
+//         sourcemap: true,
+//       },
+//     ],
+//     plugins: [
+//       resolve(),
+//       commonjs(),
+//       typescript({
+//         tsconfig: './tsconfig.json',
+//         declaration: true,
+//         // declarationDir: 'dist/types',
+//         exclude: ['**/*.test.tsx', '**/*.test.ts', '**/*.stories.ts'],
+//         declarationMap:false 
+//       }),
+        
+//       postcss({ extensions: ['.css'], inject: true, extract: false }),
+//     ],
+//   },
+//   {
+//     input: 'dist/types/esm/index.d.ts',
+//     output: [{ file: 'dist/esm/index.d.ts', format: 'esm' },
+//       { file: 'dist/cjs/index.d.ts', format: 'esm' }
+//     ],
+//     plugins: [dts()],
+//     external: [/\.css$/],
+//   },
+// ];
+
+
+export default [
+  // CommonJS Build
+  {
+    input: 'src/index.ts',
+    output: {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: 'dist/cjs',
+      }),
+      postcss({
+        extensions: ['.css'],
+        inject: true,
+        extract: false,
+      }),
+    ],
+  },
+  // ESM Build
+  {
+    input: 'src/index.ts',
+    output: {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: 'dist/esm',
+      }),
+      postcss({
+        extensions: ['.css'],
+        inject: true,
+        extract: false,
+      }),
+    ],
+  },
+  // DTS Bundler
+  {
+    input: 'dist/esm/index.d.ts', // Use one of the declaration files as input
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'esm',
+    },
+    plugins: [dts()],
+  },
+];
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react'
+
+type Props = {
+    text:string
+}
+
+const Button:FC<Props> = ( {text} ) => {
+  return (
+    <div>{text}</div>
+  )
+}
+
+export default Button
